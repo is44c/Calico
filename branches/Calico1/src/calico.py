728b84e1d47f72541f1bbc185c4873bbea2bfcd0
dblank <dblank@ed25d823-a23d-414a-8cb8-cff62a94a292>
2011-08-30 00:27:40 +0000

    Version 1.0.3

--- branches/Calico1/src/calico.py
+++ branches/Calico1/src/calico.py
@@ -621,7 +621,7 @@ class CalicoProject(object):
             actionHandler(action, **data)
 
 # Let's start!
-version = "1.0.2"
+version = "1.0.3"
 if "--help" in args:
     print()
     print(_("Calico Project, Version %s, on %s") % (version,
