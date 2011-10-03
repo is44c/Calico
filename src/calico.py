b7c4738dec781eaac635972d3e01e5cbe2ab85a2
dblank <dblank@ed25d823-a23d-414a-8cb8-cff62a94a292>
2011-10-03 16:39:46 +0000

    Version 1.1.2; new example zombies.py

--- src/calico.py
+++ src/calico.py
@@ -648,7 +648,7 @@ class CalicoProject(object):
             actionHandler(action, **data)
 
 # Let's start!
-version = "1.1.1"
+version = "1.1.2"
 if "--help" in args:
     print()
     print(_("Calico Project, Version %s, on %s") % (version,
